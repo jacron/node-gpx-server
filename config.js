e91d4f9019d9bb0c40247145e8e6165192a7270b
jhsm croonen <info@jcroonen.nl>
2020-07-09 22:58:28 +0200

    downloadfolder aangepast

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     port: 3016,
     appTitle: 'gpx server',
     activitiesMap: "/Users/orion/Documents/garminconnect/activities",
-    downloadMap: "/Volumes/Media/Download",
+    downloadMap: "/Volumes/Abeel/Download",
     activitiesXlsFile: "/Users/orion/Documents/garminconnect/csv/activities.xlsx",
     activitiesCsvFile: "/Users/orion/Documents/garminconnect/csv/Activities.csv",
 };
