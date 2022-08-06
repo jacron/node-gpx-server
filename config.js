3c37c3f5f73c17708bf33bdeef7d4e0e461a56b9
jhsm croonen <info@jcroonen.nl>
2022-08-06 09:42:18 +0200

    Update config.js
    data moved to share

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+/*
 const homedir = require('os').homedir();
+
 
 module.exports = {
     port: 3016,
@@ -8,3 +10,16 @@ module.exports = {
     activitiesCsvFile: homedir + "/Documents/garminconnect/csv/Activities.csv",
     downloadMap: "/Volumes/Abeel/Download",
 };
+*/
+
+const homedir = '/Volumes/Data/Data/garminconnect';  // require('os').homedir();
+
+module.exports = {
+    port: 3016,
+    appTitle: 'gpx server',
+    activitiesMap: homedir + "/activities",
+    activitiesXlsFile: homedir + "/csv/activities.xlsx",
+    activitiesCsvFile: homedir + "/csv/Activities.csv",
+    downloadMap: "/Volumes/Abeel/Download",
+};
+
